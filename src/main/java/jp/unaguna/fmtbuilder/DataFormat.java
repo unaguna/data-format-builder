46ad5e8149ff3d3f3ceb1b54f5215f457e69d50d
K-izumi <k.izumi.ysk@gmail.com>
2025-08-23 22:56:07 +0900

    ✨ build from printf format

--- src/main/java/jp/unaguna/fmtbuilder/DataFormat.java
+++ src/main/java/jp/unaguna/fmtbuilder/DataFormat.java
@@ -2,12 +2,14 @@ package jp.unaguna.fmtbuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * DataFormat is a formatter for data consisting of field values.
  * DataFormat.Builder can be used to build formatting rules to create a DataFormat for each project-specific data.
  */
 public class DataFormat {
+    private static final int CODE_POINT_PERCENT = '%';
     private final List<DataFormatPart> formatParts;
 
     private DataFormat(final List<DataFormatPart> formatParts) {
@@ -38,6 +40,48 @@ public class DataFormat {
             formatPart.format(toAppendTo, valueProvider);
         }
         return toAppendTo;
+    }
+
+    /**
+     * Create a DataFormat instance which formats data by printf-formatting such as '%a'.
+     *
+     * <p>
+     * The created DateFormat treats the first two characters starting with % as placeholders,
+     * and when formatting, it retrieves the value using the placeholder string as the key and embeds it.
+     * For example, if you specify ‘%abc’ as the format, the value of %a is retrieved during formatting.
+     * If this value is ‘ABC’, the result will be ‘ABCbc’. However, '%%' is an exception and '%' is embedded.
+     * </p>
+     *
+     * @param fmt printf format
+     * @return the DataFormat instance which formats data by the specified format
+     */
+    public static DataFormat fromPrintfFormat(final String fmt) {
+        Objects.requireNonNull(fmt);
+        Builder builder = new Builder();
+        int head = 0;
+        boolean percent = false;
+        while (head < fmt.length()) {
+            // the char on current head
+            final int headCodePoint = fmt.codePointAt(head);
+            if (percent) {
+                if (headCodePoint == CODE_POINT_PERCENT) {
+                    builder.constant("%");
+                } else {
+                    builder.string("%" + new String(Character.toChars(headCodePoint)));
+                }
+                percent = false;
+
+            } else {
+                if (headCodePoint == CODE_POINT_PERCENT) {
+                    percent = true;
+                } else {
+                    builder.constant(new String(Character.toChars(headCodePoint)));
+                }
+
+            }
+            head = fmt.offsetByCodePoints(head, 1);
+        }
+        return builder.build();
     }
 
     public static class Builder {
---
+++ src/test/java/jp/unaguna/fmtbuilder/DataFormatPrintfTest.java
@@ -0,0 +1,65 @@
+package jp.unaguna.fmtbuilder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DataFormatPrintfTest {
+    @Test
+    public void testPrintfBuild() {
+        final DataFormat dataFormat = DataFormat.fromPrintfFormat("abc %s_def");
+
+        final String actual = dataFormat.format(key -> {
+            if ("%s".equals(key)) {
+                return "test";
+            } else {
+                throw new UnsupportedOperationException();
+            }
+        });
+        assertEquals("abc test_def", actual);
+    }
+
+    @Test
+    public void testPrintfBuild_escape() {
+        final DataFormat dataFormat = DataFormat.fromPrintfFormat("abc%% %%%% %%s");
+
+        final String actual = dataFormat.format(key -> {
+            throw new UnsupportedOperationException();
+        });
+        assertEquals("abc% %% %s", actual);
+    }
+
+    @Test
+    public void testPrintfBuild_empty() {
+        final DataFormat dataFormat = DataFormat.fromPrintfFormat("");
+
+        final String actual = dataFormat.format(key -> {
+            throw new UnsupportedOperationException();
+        });
+        assertEquals("", actual);
+    }
+
+    @Test
+    public void testPrintfBuild_const() {
+        final DataFormat dataFormat = DataFormat.fromPrintfFormat("abcde");
+
+        final String actual = dataFormat.format(key -> {
+            throw new UnsupportedOperationException();
+        });
+        assertEquals("abcde", actual);
+    }
+
+    @Test
+    public void testPrintfBuild_2byte() {
+        final DataFormat dataFormat = DataFormat.fromPrintfFormat("\uD842\uDFB7%\uD842\uDFB7");
+
+        final String actual = dataFormat.format(key -> {
+            if ("%\uD842\uDFB7".equals(key)) {
+                return "あ";
+            } else {
+                throw new UnsupportedOperationException();
+            }
+        });
+        assertEquals("\uD842\uDFB7あ", actual);
+    }
+}
