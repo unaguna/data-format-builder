a9583f6650028f52c43045c3a8ffe8dd60892acf
K-izumi <k.izumi.ysk@gmail.com>
2025-09-15 15:41:56 +0900

    ✨ auto padding

---
+++ src/main/java/jp/unaguna/fmtbuilder/TableDataFormatIteratorWithAdapter.java
@@ -0,0 +1,89 @@
+package jp.unaguna.fmtbuilder;
+
+import java.util.*;
+
+public class TableDataFormatIteratorWithAdapter<T> implements Iterator<String> {
+    private final static int blockSize = 8192;
+    private final DataFormat baseDataFormat;
+    private final ValueProviderAdapter<T> adapter;
+    private final Iterator<T> dataIterator;
+    private final List<T> dataBuffer = new LinkedList<>();
+    private final TableFieldHolder widthProvider = new TableFieldHolder();
+
+    public TableDataFormatIteratorWithAdapter(
+            final DataFormat baseDataFormat, final Iterator<T> dataIterator, final ValueProviderAdapter<T> adapter) {
+
+        this.baseDataFormat = baseDataFormat;
+        this.dataIterator = dataIterator;
+        this.adapter = adapter;
+    }
+
+    /** イテレータから次のブロックを取得 */
+    private void loadNextBlock() {
+        if (!dataBuffer.isEmpty()) {
+            throw new IllegalStateException("cannot load next data block; buffer is not empty");
+        }
+
+        widthProvider.clear();
+
+        while (dataIterator.hasNext() && dataBuffer.size() < blockSize) {
+            final T nextData = dataIterator.next();
+            synchronized (adapter) {
+                adapter.setElement(nextData);
+                for (final String variableName : baseDataFormat.getVariableNames()) {
+                    final String value = adapter.get(variableName).toString();
+                    widthProvider.updateWidth(variableName, value.length());
+                }
+            }
+            dataBuffer.add(nextData);
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !dataBuffer.isEmpty() || dataIterator.hasNext();
+    }
+
+    @Override
+    public String next() {
+        final StringBuilder builder = new StringBuilder();
+        this.nextFormat(builder);
+        return builder.toString();
+    }
+
+    public void nextFormat(final StringBuilder builder) {
+        if (dataBuffer.isEmpty()) {
+            loadNextBlock();
+        }
+
+        // error if buffer is empty even if after loadNextBlock
+        if (dataBuffer.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        final T nextData = dataBuffer.remove(0);
+        synchronized(adapter) {
+            adapter.setElement(nextData);
+            baseDataFormat.format(adapter, widthProvider, builder);
+        }
+    }
+
+    private static class TableFieldHolder implements FieldWidthProvider {
+        private final Map<String, Integer> width = new HashMap<>();
+
+        public void clear() {
+            width.clear();
+        }
+
+        public void updateWidth(String fieldName, int minimum) {
+            if (width.getOrDefault(fieldName, 0) < minimum) {
+                width.put(fieldName, minimum);
+            }
+        }
+
+        @Override
+        public Integer getWidth(String fieldName) {
+            return width.get(fieldName);
+        }
+    }
+}
---
+++ src/test/java/jp/unaguna/fmtbuilder/TableDataFormatIteratorWithAdapterTest.java
@@ -0,0 +1,63 @@
+package jp.unaguna.fmtbuilder;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TableDataFormatIteratorWithAdapterTest {
+
+    @Test
+    public void testPadding() {
+        final DataFormat dataFormat = new DataFormat.Builder()
+                .string("key1", ValuePadding.LEFT)
+                .constant(" ")
+                .string("key2", ValuePadding.RIGHT)
+                .constant(" ")
+                .string("key3")
+                .constant(" ")
+                .string("key4")
+                .build();
+        final List<Integer> data = new ArrayList<>();
+        data.add(1);
+        data.add(2);
+        data.add(3);
+
+        final ValueProviderAdapter<Integer> adapter = new ValueProviderAdapter.Builder<Integer>()
+                .addProvider("key1", i -> "key" + repeat("1", i))
+                .addProvider("key2", i -> "key" + repeat("22", i))
+                .addProvider("key3", i -> "key" + repeat("333", i))
+                .addProvider("key4", i -> "key" + repeat("4444", i))
+                .build();
+
+        final TableDataFormatIteratorWithAdapter<Integer> tableDataFormatIterator
+                = new TableDataFormatIteratorWithAdapter<>(
+                        dataFormat,
+                        data.iterator(),
+                        adapter
+                );
+
+        final List<String> actualLines = new ArrayList<>();
+        for (int record : data) {
+            assertTrue(tableDataFormatIterator.hasNext());
+            final String line = tableDataFormatIterator.next();
+            actualLines.add(line);
+            System.out.println(line);
+        }
+        assertEquals(data.size(), actualLines.size());
+        assertEquals("  key1 key22     key333 key4444", actualLines.get(0));
+        assertEquals(" key11 key2222   key333333 key44444444", actualLines.get(1));
+        assertEquals("key111 key222222 key333333333 key444444444444", actualLines.get(2));
+    }
+
+    private String repeat(final String base, final int num) {
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            builder.append(base);
+        }
+        return builder.toString();
+    }
+}
